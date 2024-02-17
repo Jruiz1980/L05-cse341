e4a7a2ef2381adc0148db839c3e493a36fa9a7e3
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-16 23:21:15 -0500

    add changes middleware validator

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.temples = require('./customers.js')(mongoose);
 
 router.use('/customers', require('./customers'));
 router.use('/', require('./swagger'));
 
-module.exports = router;
+module.exports = router, db;
