f088c6654d1fe4e1bbd094742f22e651502f8074
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-17 11:28:25 -0500

    add validator

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,8 +1,38 @@
-const Validator = require('./validate.js');
-const validator = (body, rules, customMessages, callback) => {
-  const validation = new Validator(body, rules, customMessages);
-  validation.passes(() => callback(null, true));
-  validation.fails(() => callback(validation.errors, false));
+// En el archivo validate.js
+function Validator(body, rules, customMessages) {
+  this.body = body;
+  this.rules = rules;
+  this.customMessages = customMessages;
+  this.errors = {};
+}
+
+Validator.prototype.passes = function (callback) {
+  // Lógica de validación exitosa
+  let isValid = true;
+  this.rules.forEach((rule) => {
+    const [field, validation] = rule.split('|');
+    if (validation === 'required' && !this.body[field]) {
+      this.errors[field] = `${field} es un campo requerido.`;
+      isValid = false;
+    }
+  });
+
+  if (isValid) {
+    callback(null, true);
+  } else {
+    callback(this.errors, false);
+  }
 };
 
-module.exports = validator;
+Validator.prototype.fails = function (callback) {
+  // Lógica de validación fallida
+  this.passes((errors, status) => {
+    if (!status) {
+      callback(errors, false);
+    } else {
+      callback(null, true);
+    }
+  });
+};
+
+module.exports = Validator;
