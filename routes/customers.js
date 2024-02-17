0ecd10717012c66c88c3b7c524ad1f7120313af4
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-17 00:15:55 -0500

    add validation in routes

--- routes/customers.js
+++ routes/customers.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const customersController = require('../controllers/customers');
+const validation = require('../middleware/validate');
 
 router.get('/', customersController.getAll);
 
 router.get('/:id', customersController.getSingle);
 
-router.post('/', customersController.createCustomer);
+router.post('/', validation.customersController.createCustomer);
 
-router.put('/:id', customersController.updateCustomer);
+router.put('/:id', validation.customersController.updateCustomer);
 
 router.delete('/:id', customersController.deleteCustomer);
 
