5340b7c76aaee0a92345a79ef6efaa900a73e642
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-16 23:49:50 -0500

    add changes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.temples = require('./customers.js')(mongoose);
 
 router.use('/customers', require('./customers'));
 router.use('/', require('./swagger'));
 
-module.exports = router, db;
+module.exports = router;
