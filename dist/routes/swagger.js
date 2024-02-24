d37d0f3922e99de703f506e652d41480def7bb15
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-24 04:37:57 -0500

    delete

--- dist/routes/swagger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
-const router = express.Router();
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
-exports.default = router;
