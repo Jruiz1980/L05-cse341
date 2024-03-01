1f88f3017d17ff45e75a65f16e46f596af0eba71
Jruiz1980 <johnathanruiz@gmail.com>
2024-03-01 02:13:54 -0500

    add changes

--- dist/middleware/validate.js
+++ dist/middleware/validate.js
@@ -1,37 +1,35 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleValidationErrors = exports.validations = void 0;
-const express_validator_1 = require('express-validator');
+const express_validator_1 = require("express-validator");
 const validateFirstName = (0, express_validator_1.body)('firstName')
-  .notEmpty()
-  .withMessage('The first name field is required.');
+    .notEmpty()
+    .withMessage('The first name field is required.');
 const validateLastName = (0, express_validator_1.body)('lastName')
-  .notEmpty()
-  .withMessage('The last name field is required.');
-const validateEmail = (0, express_validator_1.body)('email')
-  .isEmail()
-  .withMessage('The email field must be a valid email.');
+    .notEmpty()
+    .withMessage('The last name field is required.');
+const validateEmail = (0, express_validator_1.body)('email').isEmail().withMessage('The email field must be a valid email.');
 const validateAddress = (0, express_validator_1.body)('address').optional().isString();
 const validateStoreName = (0, express_validator_1.body)('storeName')
-  .notEmpty()
-  .withMessage('The store name field is required.');
+    .notEmpty()
+    .withMessage('The store name field is required.');
 const validations = [
-  validateFirstName,
-  validateLastName,
-  validateEmail,
-  validateAddress,
-  validateStoreName
+    validateFirstName,
+    validateLastName,
+    validateEmail,
+    validateAddress,
+    validateStoreName
 ];
 exports.validations = validations;
 const handleValidationErrors = (req, res, next) => {
-  const errors = (0, express_validator_1.validationResult)(req);
-  if (!errors.isEmpty()) {
-    return res.status(412).send({
-      success: false,
-      message: 'Validation failed',
-      data: errors.array()
-    });
-  }
-  next();
+    const errors = (0, express_validator_1.validationResult)(req);
+    if (!errors.isEmpty()) {
+        return res.status(412).send({
+            success: false,
+            message: 'Validation failed',
+            data: errors.array()
+        });
+    }
+    next();
 };
 exports.handleValidationErrors = handleValidationErrors;
--- dist/server.js
+++ dist/server.js
@@ -1,31 +1,32 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
-const routes_1 = require('./routes');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+const routes_1 = require("./routes");
 const port = process.env.PORT || 8080;
 const app = express();
 app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
+    .use(bodyParser.json())
+    .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
-  .use('/', routes_1.default);
+})
+    .use('/', routes_1.default);
 app.use((err, req, res, next) => {
-  console.error('Error message: ', err.message);
-  console.error('Stack trace: ', err.stack);
-  res.status(500).send('Something broke!');
+    console.error('Error message: ', err.message);
+    console.error('Stack trace: ', err.stack);
+    res.status(500).send('Something broke!');
 });
 process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
 });
