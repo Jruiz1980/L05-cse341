6f778116342f0981049dafb418fb6b10a131afad
Jruiz1980 <johnathanruiz@gmail.com>
2024-03-12 05:33:52 -0500

    add validate

--- middleware/validateSeller.js
+++ middleware/validateSeller.js
@@ -1,13 +1,17 @@
 const mongodb = require('../db/connect');
-const user = require('../routes/user');
-const { ObjectId } = require('mongodb');
 
 const validateSeller = async (req, res, next) => {
-  const userId = req.user.id; // Assuming you have the user's ID from the OAuth token
+  // Asumiendo que el email del usuario está disponible en req.user.email
+  const userEmail = req.user?.email; // Uso de optional chaining por si req.user es undefined
+
+  if (!userEmail) {
+    return res.status(401).json({ message: 'No se ha podido verificar la identidad del usuario.' });
+  }
+
   try {
-    const seller = await mongodb.getDb().db().collection('sellers').findOne({ _id: new ObjectId(userId) });
-    if (!seller) {
-      return res.status(403).json({ message: 'Access denied. User is not a seller.' });
+    const seller = await mongodb.getDb().db().collection('sellers').findOne({ email: userEmail });
+    if (!seller) { // Verifica si existe un registro de vendedor con ese email
+      return res.status(403).json({ message: 'Acceso denegado. El usuario no está autorizado como vendedor.' });
     }
     next();
   } catch (err) {
