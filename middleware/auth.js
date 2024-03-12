4581c10c0ecee0a5562dbf54f5fd83b5c6b8b41f
Jruiz1980 <johnathanruiz@gmail.com>
2024-03-12 17:21:31 -0500

    Add auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 // middleware/auth.js
 function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
+    req.seller = rqe.email.seller;
     return next();
   }
   res.status(401).send({ message: 'Do not authorized' });
