a3516af1313b352bb07db8865de28138d4203d29
Jruiz1980 <johnathanruiz@gmail.com>
2024-03-13 00:02:47 -0500

    fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 // middleware/auth.js
 function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    req.seller = rqe.email.seller;
+    req.seller = req.email.seller;
     return next();
   }
   res.status(401).send({ message: 'Do not authorized' });
